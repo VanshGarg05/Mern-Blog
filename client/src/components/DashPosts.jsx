179f17337c8a808af6476f44e2ea6c1ff7474233
Vansh <vanshgarg05@gmail.com>
2025-06-18 05:23:43 +0530

    Completing the Post display section

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 'flowbite-react'
+import {Link} from 'react-router-dom'
 
 export default function DashPosts() {
+  const { currentUser } = useSelector((state)=>state.user)
+  const [userPosts,setUserPosts] = useState([])
+  useEffect(()=>{
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`/api/post/getPosts?userId=${currentUser.data._id}`)
+        const data = await res.json()
+
+        if(res.ok){
+          console.log(data);
+          setUserPosts(data.data.posts)
+        }
+        
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    if(currentUser.data.isAdmin) {
+      fetchPosts()
+    }
+  },[currentUser.data._id])
   return (
-    <div>DashPosts</div>
+    <div>
+      {currentUser.data.isAdmin && userPosts.length > 0 ? (
+        <>
+        <Table hoverable className='shadow-md'>
+          <TableHead>
+            <TableHeadCell>Date updated</TableHeadCell>
+            <TableHeadCell>Post Image</TableHeadCell>
+            <TableHeadCell>Post Title</TableHeadCell>
+            <TableHeadCell>Category</TableHeadCell>
+            <TableHeadCell>Delete</TableHeadCell>
+            <TableHeadCell>
+              <span className=''>Edit</span>
+            </TableHeadCell>
+          </TableHead>
+          {userPosts.map((post)=>(
+            <TableBody>
+              <TableRow>
+                <TableCell> {new Date(post.updatedAt).toLocaleDateString()} </TableCell>
+                <TableCell>
+                  <Link to={`post/${post.slug}`}>
+                    <img src='post.image' alt="post.title" className='w-20 h-10 object-cover bg-gray-500' />
+                  </Link>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+      ))}
+        </Table>
+        </>
+      ) : (
+        <p>You have no Posts yet!</p>
+      )}
+    </div>
   )
 }
+
